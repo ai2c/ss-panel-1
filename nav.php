ba5a504dbb73e7972ca68fd54549fecf12b6d665
Yang Li <liyangyijie@users.noreply.github.com>
2015-04-10 01:11:33 +0800

    Update nav.php

--- nav.php
+++ nav.php
@@ -3,7 +3,7 @@
         <li role="presentation"><a href="index.php">主页</a></li>
         <li role="presentation"><a href="user/login.php">登录</a></li>
         <li role="presentation"><a href="user/reg.php">注册</a></li>
-        <li role="presentation"><a href="user">用户中心</a></li>
+        <li role="presentation"><a href="user/index.php">用户中心</a></li>
     </ul>
     <h3 class="text-muted"><?php echo $site_name; ?></h3>
 </div>
