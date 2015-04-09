5829ebd5c6599bded88e8f451441262815e118df
Yang Li <liyangyijie@users.noreply.github.com>
2015-04-10 02:11:51 +0800

    Update config-simple.php

--- lib/config-simple.php
+++ lib/config-simple.php
@@ -13,7 +13,7 @@ $togb = $tomb*1024;
 
 //Define DB Connection  数据库信息
 define('DB_HOST','localhost');
-define('DB_USER','root');
+define('DB_USER','shadowsocks');
 define('DB_PWD','password');
 define('DB_DBNAME','db');
 define('DB_CHARSET','utf8');
@@ -24,23 +24,23 @@ define('DB_TYPE','mysql');
 
 //define Plan
 //注册用户的初始化流量
-//默认5GiB
-$a_transfer = $togb*5;
+//默认30GiB
+$a_transfer = $togb*30;
 
 //签到设置 签到活的的最低最高流量,单位MB
-$check_min = 1;
-$check_max = 100;
+$check_min = 0;
+$check_max = 0;
 
 //name
-$site_name = "ss-panel";
-$site_url  = "https://panel.com/";
+$site_name = "Our Private Panel";
+$site_url  = "http://panel.com/";
 
 //invite only
 $invite_only = true;
 //用户注册后获得的邀请码最低最高
 //都设置为0用户就不能邀请
-$user_invite_min = '1';
-$user_invite_max = '1';
+$user_invite_min = '0';
+$user_invite_max = '0';
 
 //mail-gun
 // Get your key from https://mailgun.com
