b160b1ab42dfa2210ae9a96b2befa31f8a1c8977
Yang Li <liyangyijie@users.noreply.github.com>
2015-04-14 22:03:55 +0800

    Update node.php

--- user/node.php
+++ user/node.php
@@ -70,47 +70,7 @@ include_once 'lib/slidebar_left.inc.php';
                 </div><!-- /.box -->
             </div><!-- /.col (left) -->
 
-            <div class="col-md-6">
-                <div class="box box-solid">
-                    <div class="box-header">
-                        <i class="fa fa-code"></i>
-                        <h3 class="box-title">Pro节点</h3>
-                    </div><!-- /.box-header -->
-                    <div class="box-body">
-                        <div class="callout callout-warning">
-                            <h4>注意!</h4>
-                            <p>...</p>
-                        </div><?php
-                        $node1 = $node->get_node_array(1);
-                        foreach($node1 as $row){
-                            ?>
-                            <div class="nav-tabs-custom">
-                                <ul class="nav nav-tabs pull-right">
-                                    <li class="dropdown">
-                                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                                            操作 <span class="caret"></span>
-                                        </a>
-                                        <ul class="dropdown-menu">
-                                            <li role="presentation"><a role="menuitem" target="_blank" tabindex="-1" href="node_json.php?id=<?php echo $row['id']; ?>">配置文件</a></li>
-                                            <li role="presentation"><a role="menuitem" target="_blank" tabindex="-1" href="node_qr.php?id=<?php echo $row['id']; ?>">二维码</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="pull-left header"><i class="fa fa-angle-right"></i> <?php echo $row['node_name']; ?></li>
-                                </ul>
-                                <div class="tab-content">
-                                    <div class="tab-pane active" id="tab_1-1">
-                                        <p> <a class="btn btn-xs bg-purple btn-flat margin" href="#">地址:</a> <code><?php echo $row['node_server']; ?></code>
-                                            <a class="btn btn-xs bg-orange btn-flat margin" href="#"><?php echo $row['node_status']; ?></a>
-                                            <a class="btn btn-xs bg-green btn-flat margin" href="#"><?php echo $row['node_method']; ?></a>
-                                        </p>
-                                        <p> <?php echo $row['node_info']; ?></p>
-                                    </div><!-- /.tab-pane -->
-                                </div><!-- /.tab-content -->
-                            </div><!-- nav-tabs-custom -->
-                        <?php }?>
-                    </div><!-- /.box-body -->
-                </div><!-- /.box -->
-            </div><!-- /.col (right) --> 
+    
         </div><!-- /.row -->
         <!-- END PROGRESS BARS -->
     </section><!-- /.content -->
