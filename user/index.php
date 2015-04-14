b143672f3faea6f363555597cd4f8bb952d9cc04
Yang Li <liyangyijie@users.noreply.github.com>
2015-04-14 22:44:20 +0800

    Update index.php

--- user/index.php
+++ user/index.php
@@ -53,7 +53,7 @@ $unix_time = $oo->get_last_unix_time();
                         <div class="box-body">
                             <p> 端口: <code><?php echo $oo->get_port();?></code> </p>
                             <p> 密码: <?php echo $oo->get_pass();?> </p>
-                            <p> 套餐: <span class="label label-info"> <?php echo $oo->get_plan();?> </span> </p>
+                    <!--    <p> 套餐: <span class="label label-info"> <?php echo $oo->get_plan();?> </span> </p>  -->
                             <p> 最后使用时间: <code><?php echo date('Y-m-d H:i:s',$unix_time);  ?></code> </p>
                         </div><!-- /.box-body -->
                     </div><!-- /.box -->
